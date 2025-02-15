76aeb97b8a9f07cfdd31b21672481f2ee65b2fcf
Cedric Agoliki <agolikicedric@gmail.com>
2025-02-15 18:32:27 +0000

    Improve layout, add address text input

--- main.go
+++ main.go
@@ -5,8 +5,20 @@ import (
 )
 
 func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+	contentBox := tview.NewBox().SetBorder(true).SetTitle("TEXTNET")
+
+	addressInput := tview.NewInputField().SetPlaceholder("Enter address")
+
+	inputFlex := tview.NewFlex().
+		SetDirection(tview.FlexColumn).
+		AddItem(addressInput, 0, 1, false)
+
+	flex := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(contentBox, 0, 1, false).
+		AddItem(inputFlex, 1, 0, true)
+
+	if err := tview.NewApplication().SetRoot(flex, true).Run(); err != nil {
 		panic(err)
 	}
 }
