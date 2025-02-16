7e20427fd01b8f04073227e2ca27e655d2b1e6ba
Cédric Agoliki <agolikicedric@gmail.com>
2025-02-16 15:18:17 +0000

    input loading logic

--- main.go
+++ main.go
@@ -1,24 +1,48 @@
 package main
 
 import (
+	"time"
+
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func main() {
+	app := tview.NewApplication()
 	contentBox := tview.NewBox().SetBorder(true).SetTitle("TEXTNET")
 
-	addressInput := tview.NewInputField().SetPlaceholder("Enter address")
+	cmdInput := tview.NewInputField().
+		SetPlaceholder("Enter address")
+
+	cmdInput.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			// loadSite(contentBox)
+			go func() {
+				address := cmdInput.GetText()
+				contentBox.SetTitle("Loading " + address)
+				cmdInput.SetDisabled(true)
+				app.ForceDraw()
+
+				// should make request here
+				time.Sleep(5 * time.Second)
+
+				contentBox.SetTitle("TEXTNET")
+				cmdInput.SetDisabled(false)
+				app.ForceDraw()
+			}()
+		}
+	})
 
 	inputFlex := tview.NewFlex().
 		SetDirection(tview.FlexColumn).
-		AddItem(addressInput, 0, 1, false)
+		AddItem(cmdInput, 0, 1, false)
 
 	flex := tview.NewFlex().
 		SetDirection(tview.FlexRow).
 		AddItem(contentBox, 0, 1, false).
 		AddItem(inputFlex, 1, 0, true)
 
-	if err := tview.NewApplication().SetRoot(flex, true).SetFocus(addressInput).Run(); err != nil {
+	if err := app.SetRoot(flex, true).SetFocus(cmdInput).Run(); err != nil {
 		panic(err)
 	}
 }
